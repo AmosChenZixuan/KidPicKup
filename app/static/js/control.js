a075744622221a3793ad438316dd40c97c005292
Amos <zixuanc2@andrew.cmu.edu>
2022-11-19 01:20:12 -0500

    remove console log

--- app/static/js/control.js
+++ app/static/js/control.js
@@ -1,13 +1,9 @@
 function showBtn(group, btn_id){
-    console.log('hovering')
-    console.log("btn_"+group+"_"+btn_id)
     const btn = document.getElementById("btn_"+group+"_"+btn_id)
     btn.style.opacity = 1
 }
     
 function hideBtn(group, btn_id){
-    console.log('leaving')
-    console.log("btn_"+group+"_"+btn_id)
     const btn = document.getElementById("btn_"+group+"_"+btn_id)
     btn.style.opacity = 0
 }
