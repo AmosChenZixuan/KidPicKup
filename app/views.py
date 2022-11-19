53a044474c67abbbae197c6e940ed590a38651d7
Amos <zixuanc2@andrew.cmu.edu>
2022-11-19 00:08:59 -0500

    fix student name and button alignment

--- app/views.py
+++ app/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from rest_framework.decorators import api_view, permission_classes
-from django.core.paginator import Paginator
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
+from .models import Student, Vehicle
 import json
 
 
 
-
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'ClassA': [],
+        'ClassB': [],
+    }
+    students = Student.objects.all()
+    for student in students:
+        if student.status == 0: # should be 1
+            data = str(student)
+            if student.classes == 0:
+                context['ClassA'].append(data)
+            else:
+                context['ClassB'].append(data)
+
+    return render(request, 'index.html', context)
 
 def home(request, model):
     context = {}
-
-    if request.method == 'GET':
-
-        return render(request, 'home.html', context)
-
-def retrain(request):
-    if request.method == 'POST':
-        params = json.loads(request.body)
-
-        return HttpResponse(status=200)
-    return HttpResponse(status=400)
